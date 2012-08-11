bd832e42a1e6318ca70f37a7632554bc9edc5996
Vipin Nair <swvist@gmail.com>
2012-08-12 03:15:37 +0530

    Adding a trigger on total load time change in local storage.

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,7 @@ $(function () {
 	MyApp.top = new MyApp.Views.TopSitesView();
 	MyApp.activity = new MyApp.Views.PagesPerTimeView();
 	MyApp.data = new MyApp.Models.RawData();
+	MyApp.data.on('LT',function(){
+		console.log("Triggered. Load Time : ", localStorage.analytics_loadTime);
+	});
 });
