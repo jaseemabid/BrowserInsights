0e81fde4c7235b0fc1201fdb9d96595d2654a6c1
Vipin Nair <swvist@gmail.com>
2012-08-12 03:07:58 +0530

    Removing a console log

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -5,6 +5,6 @@ else{
 	chrome.webNavigation.onCompleted.addListener(function(data){
 		var p = window.performance.timing;
 		localStorage.analytics_loadTime=(p.domComplete-p.navigationStart)+parseInt(localStorage.analytics_loadTime);
-		console.log(localStorage.analytics_loadTime);
+		MyApp.data.trigger("LT");
 	});
 };
