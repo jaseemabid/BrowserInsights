26d2c53229112b7cd811a727ecd6601f70378b01
Jaseem Abid <jaseemabid@gmail.com>
2012-08-11 23:51:29 +0530

    Implementing PagesPerTimeView and TopSitesView

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 // App init
+
+/*globals MyApp, Backbone, $, _ */
+
+$(function () {
+	"use strict";
+	MyApp.top = new MyApp.Views.TopSitesView();
+	MyApp.activity = new MyApp.Views.PagesPerTimeView();
+});
--- src/js/views.js
+++ src/js/views.js
@@ -1,10 +1,88 @@
 /* Backbone.js views */
 
-/*globals MyApp, Backbone */
+/*globals MyApp, Backbone, $, _ */
 
+"use strict";
 MyApp.Views = {
 	TopSitesView : Backbone.View.extend({
+		id : "inlineGraph1",
+		title : "Top visited sites",
+		initialize : function () {
+			this.render();
+		},
+		_data : function () {
+			var data = [[
+				['google.com', 12],
+				['facebook.com', 9],
+				['hackernews', 14],
+				['en.wikipedia.com', 16],
+				['twitter.com', 7],
+				['Yahoo!', 9]
+			]];
+			return data;
+		},
+		render : function () {
+
+			var that = this,
+				plot = $.jqplot(that.id, that._data(), {
+					title : that.title,
+					textColor : "#ff0000",
+					grid : {
+						drawGridLines	: false,
+						shadow			: false,
+						borderWidth		: 0,
+						background		: '#efefef',
+						borderColor		: '#fffff'
+					},
+					seriesDefaults: {
+						// Make this a pie chart.
+						renderer: $.jqplot.PieRenderer,
+						rendererOptions : {
+							showDataLabels : true
+						}
+					},
+					legend : {
+						show			: true,
+						location		: 'e'
+					}
+				});
+
+		}
 	}),
 	PagesPerTimeView : Backbone.View.extend({
+		id : "bigNumPlot",
+		className : "offset2 span4 small",
+		title : "Pages visited per minute",
+		_data : function () {
+			return [[3, 7, 9, 1, 4, 6, 8, 2, 5]];
+		},
+		initialize : function () {
+			console.log("Bind event here");
+			this.render();
+		},
+		render : function () {
+			var plot = $.jqplot(this.id, this._data(), {
+				title : this.title,
+				grid : {
+					drawGridLines	: false,
+					shadow			: false,
+					borderWidth		: 0,
+					background		: '#efefef',
+					borderColor		: '#fffff'
+				},
+				axes : {
+					xaxis : {
+						renderer			: $.jqplot.CategoryAxisRenderer,
+						showTickMarks		: false,
+						showTicks			: false
+					},
+					yaxis : {
+						showTickMarks : true,
+						showTicks : true
+					}
+				}
+			});
+			$("div#bigNumGraph span.num").html(_.last(this._data()[0]));
+		}
 	})
 };
